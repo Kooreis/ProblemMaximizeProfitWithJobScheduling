c8467995ecc2dfce22ff4a320e0982978d1ad0f6
Kooreis <kooreis.official@gmail.com>
2024-05-30 16:03:36 -0500

    This update adds the printJobScheduling function, which sorts the jobs in descending order of profit.

--- solution.py
+++ solution.py
@@ -1,5 +1,6 @@
-class Job:
-    def __init__(self, id, deadline, profit):
-        self.id = id
-        self.deadline = deadline
-        self.profit = profit
+def printJobScheduling(jobs, t):
+    n = len(jobs)
+    for i in range(n):
+        for j in range(n - 1 - i):
+            if jobs[j].profit < jobs[j + 1].profit:
+                jobs[j], jobs[j + 1] = jobs[j + 1], jobs[j]
