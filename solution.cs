2e1ca5433ecc348ab43344c00c82a552397f5318
Kooreis <kooreis.official@gmail.com>
2024-05-30 16:03:36 -0500

    This update adds the Main method, which creates a list of jobs and calls the ScheduleJobs method to calculate the maximum obtainable profit.

--- solution.cs
+++ solution.cs
@@ -1,10 +1,15 @@
-using System;
-using System.Linq;
-using System.Collections.Generic;
-
-public class Job
+public class Program
 {
-    public char Id { get; set; }
-    public int Deadline { get; set; }
-    public int Profit { get; set; }
-}
+    public static void Main()
+    {
+        List<Job> jobs = new List<Job>()
+        {
+            new Job { Id = 'a', Deadline = 2, Profit = 100 },
+            new Job { Id = 'b', Deadline = 1, Profit = 19 },
+            new Job { Id = 'c', Deadline = 2, Profit = 27 },
+            new Job { Id = 'd', Deadline = 1, Profit = 25 },
+            new Job { Id = 'e', Deadline = 3, Profit = 15 },
+        };
+
+        Console.WriteLine("Maximum obtainable profit is " + ScheduleJobs(jobs, 3));
+    }
